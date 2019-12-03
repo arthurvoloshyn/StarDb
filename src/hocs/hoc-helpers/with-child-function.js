16a4899ceb4c91b6d29a0c797c7a6b5bf8bed832
Artur <grandfaraon93@gmail.com>
2019-12-03 16:50:33 +0200

    hoc update

--- src/hocs/hoc-helpers/with-child-function.js
+++ src/hocs/hoc-helpers/with-child-function.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const withChildFunction = (fn) => (Wrapped) => (props) => (
+const withChildFunction = (fn) => (Wrapped) => (props => (
   <Wrapped {...props}>
     {fn}
   </Wrapped>
-);
+));
 
 export default withChildFunction;
