b09c62645e23bd556a44150fa26adc33af904def
Artur <grandfaraon93@gmail.com>
2020-01-01 16:46:19 +0200

    Updates

---
+++ precache-manifest.f527dc15ec46c31158ee9692f1565efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d19db7963d011dc6e7",
+    "url": "/ReactStarDb/static/js/runtime~main.8170ac29.js"
+  },
+  {
+    "revision": "51ffb0e2a08b200d1a79",
+    "url": "/ReactStarDb/static/js/main.98724250.chunk.js"
+  },
+  {
+    "revision": "5dd53827c28192dc8545",
+    "url": "/ReactStarDb/static/js/2.e5c75611.chunk.js"
+  },
+  {
+    "revision": "51ffb0e2a08b200d1a79",
+    "url": "/ReactStarDb/static/css/main.e7690d5a.chunk.css"
+  },
+  {
+    "revision": "ef18f63d7be3e4cfab729532c376b401",
+    "url": "/ReactStarDb/index.html"
+  }
+];
