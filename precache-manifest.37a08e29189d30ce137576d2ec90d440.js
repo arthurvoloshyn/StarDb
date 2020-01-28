8ec4d7fea6ea5d965ae0949fb94d01e900afc487
Artur <grandfaraon93@gmail.com>
2020-01-28 16:47:50 +0200

    Updates

---
+++ precache-manifest.37a08e29189d30ce137576d2ec90d440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a21adeb7c9d0b8cf40f",
+    "url": "/StarDb/static/js/runtime~main.6291c308.js"
+  },
+  {
+    "revision": "9bcab39d11e27175ead9",
+    "url": "/StarDb/static/js/main.ab21b171.chunk.js"
+  },
+  {
+    "revision": "5dd53827c28192dc8545",
+    "url": "/StarDb/static/js/2.e5c75611.chunk.js"
+  },
+  {
+    "revision": "9bcab39d11e27175ead9",
+    "url": "/StarDb/static/css/main.e7690d5a.chunk.css"
+  },
+  {
+    "revision": "6a0aeb59fa888e8fea460f974664de06",
+    "url": "/StarDb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactStarDb/precache-manifest.7f7b9f016817e19f7466ecba0f13df25.js"
+  "/StarDb/precache-manifest.37a08e29189d30ce137576d2ec90d440.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactStarDb/index.html", {
+workbox.routing.registerNavigationRoute("/StarDb/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
